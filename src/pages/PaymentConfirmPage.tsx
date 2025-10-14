3e5e382411f381fd89cf0b44eb7808ee8ab215bf
unknown <datp1907@gmail.com>
2025-10-14 12:47:30 +0700

    ned

---
+++ src/pages/PaymentConfirmPage.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+import { ENV } from "@/config/ENV";
+import { calculateVnpSecureHash } from "@/utils/calculateVnpSecureHash";
+import { sortObject } from "@/utils/sortObject";
+
+// Định nghĩa kiểu cho các tham số query
+interface VnpParams {
+  [key: string]: string | undefined;
+}
+
+const PaymentConfirmPage: React.FC = () => {
+  const location = useLocation();
+  const queries = queryString.parse(location.search) as VnpParams;
+  const vnp_HashSecret = ENV.vnp_HashSecret || "";
+  const navigate = useNavigate();
+
+  const verifyPayment = async () => {
+    const { vnp_SecureHash, ...vnp_Params } = queries;
+    const sortedParams = sortObject(vnp_Params)
+      .map(
+        (key: string) =>
+          `${key}=${encodeURIComponent(vnp_Params[key] as string)}`
+      )
+      .join("&");
+    const signed = calculateVnpSecureHash(sortedParams, vnp_HashSecret);
+    if (vnp_SecureHash === signed) {
+      const { vnp_TransactionStatus } = vnp_Params;
+      if (vnp_TransactionStatus === "00") {
+        alert("Thanh toán thành công");
+        navigate("/");
+      } else {
+        alert("Thanh toán không thành công");
+        navigate("/thanh-toan-that-bai");
+      }
+    } else {
+      alert("Xác thực thanh toán thất bại");
+      navigate("/thanh-toan-that-bai");
+    }
+  };
+
+  useEffect(() => {
+    if (queries.vnp_SecureHash) {
+      verifyPayment();
+    }
+  }, []);
+
+  return (
+    <div className="flex mobile:flex-col w-full h-screen bg-white p-5">
+      <div className="flex mobile:w-full w-6/12 mx-auto justify-center items-center ">
+        <div className="w-[400px]">
+          <video
+            className="object-cover"
+            src="https://cdnl.iconscout.com/lottie/premium/thumb/payment-received-8453779-6725893.mp4"
+            autoPlay
+            loop
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentConfirmPage;
